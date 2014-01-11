cd5c35e6c30bf412fe41184e168227b6ea14753f
Laurent Decorps <ldecorps@gmail.com>
2014-01-11 18:45:41 +0000

    refactoring

--- EventProcessor/src/test/java/decorps/eventprocessor/snipets/ProgramComboSwitcher.java
+++ EventProcessor/src/test/java/decorps/eventprocessor/snipets/ProgramComboSwitcher.java
@@ -19,34 +19,25 @@ public class ProgramComboSwitcher {
 		Receiver tetraReceiver = null;
 		for (Info info : LinkFactory.midiDeviceInfos) {
 			if (!info.getName().equals("DSI Tetra - DSI Tetra"))
-				// if (!info.getName().equals(
-				// "USB Uno MIDI Interface - USB Uno MIDI Interface"))
 				continue;
 			MidiDevice tetra = MidiSystem.getMidiDevice(info);
 			if (tetra.getMaxReceivers() == 0)
 				continue;
-			// tetra.open();
 			tetraReceiver = tetra.getReceiver();
 		}
 		byte progChange = (byte) 0x30;
 		byte comboChange = (byte) 0x31;
 		byte switchTo = progChange;
-		SysexMessage EOX = new SysexMessage();
-		EOX.setMessage(0xF7, new byte[] {}, 0);
 		while (true) {
 			SysexMessage sysexMessage = new SysexMessage();
 			switchTo = switchTo == progChange ? comboChange : progChange;
 			try {
 				sysexMessage.setMessage(0xF0, new byte[] { (byte) 0x01,
 						(byte) 0x26, switchTo, (byte) (0xF7 & 0xFF) }, 4);
-
-				// (byte) 0xF7
 			} catch (InvalidMidiDataException e) {
 				e.printStackTrace();
 			}
 			tetraReceiver.send(sysexMessage, -1);
-			// tetraReceiver.send(EOX, -1);
-
 			System.out.println(Arrays.asList(sysexMessage.getData()));
 			Thread.sleep(1000);
 		}
